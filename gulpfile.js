3818747b661b3faaa3bd658548032550795c961f
Graham Marlow <mgmarlow@gmail.com>
2017-01-02 19:18:42 -0800

    Remove build/ dir transpilation step

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,15 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     gutil = require('gulp-util');
 
-
 var styleTasks = [];
 gulp.task('sass', function () {
     gulp.src('./styles/sass/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./styles/build/'));
-});
-styleTasks.push('sass');
-
-gulp.task('concat', function () {
-    return gulp.src('./styles/build/*.css')
+        .pipe(gulp.dest('./styles/build/'))
         .pipe(concat('styles.css'))
         .pipe(gulp.dest('./styles/'));
 });
-styleTasks.push('concat');
+styleTasks.push('sass');
 
 gulp.task('watch-sass', function () {
     gulp.watch(['./styles/sass/**/*.scss', './styles/build/*css'], styleTasks);
