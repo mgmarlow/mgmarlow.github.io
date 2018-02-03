10e56adbff243856c0e55584cebe60993ba45e05
Graham Marlow <mgmarlow@gmail.com>
2018-02-03 15:51:50 -0800

    add interactive background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl:'./app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    particlesJS.load('root', './assets/particles.json');
+  }
 }
