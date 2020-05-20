849562ea9039ea12e248cc82bc4af3ba43236e87
Graham Marlow <mgmarlow@gmail.com>
2020-05-19 17:55:33 -0700

    Migrate index.css to styled

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
-import Header from './header'
-import Container from './Container'
-
-import './index.css'
+import Header from '../header'
+import Container from '../Container'
+import DefaultTheme from './DefaultTheme'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -18,10 +17,12 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <Container>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <main>{children}</main>
-    </Container>
+    <DefaultTheme>
+      <Container>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <main>{children}</main>
+      </Container>
+    </DefaultTheme>
   )
 }
 
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Media from 'react-media'
 import { graphql, Link } from 'gatsby'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import styles from './blog.module.css'
 
