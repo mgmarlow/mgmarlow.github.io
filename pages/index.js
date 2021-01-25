bec6b5a9c8b35287c597f3c80cd707e4d805b1ff
Graham Marlow <mgmarlow@hey.com>
2021-01-24 17:54:43 -0800

    embolden index title

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,8 @@ export default function Home() {
 
       <Container>
         <article>
-          <h1 className="is-size-3">Graham Marlow</h1>
+          <h1 className="is-size-3 has-text-weight-bold">Graham Marlow</h1>
+
           <P>
             I am a software engineer currently working at{' '}
             <Emphasis variant="blue" href="https://mindbody.io/">
